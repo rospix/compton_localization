f95982117d07b4ae685b56706535a81eaca044f2
Tomas Baca <klaxalk@gmail.com>
2021-06-09 10:09:53 +0200

    minor changes

--- src/compton_localization.cpp
+++ src/compton_localization.cpp
@@ -387,7 +387,7 @@ mrs_msgs::Reference ComptonLocalization::generateTrackingReference(void) {
   new_reference.position.x = radiation_pose.pose.pose.position.x + params_.tracking_radius * cos(current_angle + 1.57);
   new_reference.position.y = radiation_pose.pose.pose.position.y + params_.tracking_radius * sin(current_angle + 1.57);
   new_reference.position.z = params_.tracking_height;
-  new_reference.heading = atan2(radiation_pose.pose.pose.position.y - new_reference.position.y, radiation_pose.pose.pose.position.x - new_reference.position.x);
+  new_reference.heading = atan2(radiation_pose.pose.pose.position.y - new_reference.position.y, radiation_pose.pose.pose.position.x - new_reference.position.x) - 1.57;
 
   ROS_INFO_THROTTLE(1.0, "[ComptonLocalization]: current angle: %.2f", current_angle);
 
